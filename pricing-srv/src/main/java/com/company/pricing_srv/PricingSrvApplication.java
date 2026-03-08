d0a9bff0baf7872df026632ae66b7b1ca2aa34d4
Marta Pangusión <marta.pangusion.contractor@openpay.com.es>
2026-03-08 19:37:39 +0100

    chore: initialize spring boot project

---
+++ pricing-srv/src/main/java/com/company/pricing_srv/PricingSrvApplication.java
@@ -0,0 +1,13 @@
+package com.company.pricing_srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingSrvApplication.class, args);
+	}
+
+}
---
+++ pricing-srv/src/test/java/com/company/pricing_srv/PricingSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.pricing_srv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
